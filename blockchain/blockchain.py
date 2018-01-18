886dde59e78cfa09df8b5997ce88a754621c951e
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-01-19 01:18:25 +0900

    Add valid_chain method

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -113,6 +113,36 @@ class Blockchain(object):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A Blockchain
+        :return: <bool> True is valid, False if not
+        """
+
+        last_block  = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f"{last_block}")
+            print(f"{block}")
+            print("\n----------------------------------\n")
+
+            # Check that the hash of the block is correct
+            if block["previous_hash"] != self.hash(last_block):
+                return False
+
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block["proof"], block["proof"])
+                return False
+
+            last_block = block
+            current_index += 1
+        return True
+
+
+
 
 
 
