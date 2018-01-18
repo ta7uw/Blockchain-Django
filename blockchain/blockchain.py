d3039bbdc4117ed6b111722a8641fe8ede92b3e6
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-01-19 00:17:17 +0900

    Add hash  method

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -54,5 +54,19 @@ class Blockchain(object):
 
         return self.last_block["index"] + 1
 
+    @staticmethod
+    def hash(block):
+        """
+        Create a SHA256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is orderd, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+
+
 
 
