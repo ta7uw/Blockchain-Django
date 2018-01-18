f0d6618ea9ebd9e2d6bd18f58e802ffe94af51ea
Shimbo Takaaki <ta7uwtaka@gmail.com>
2018-01-18 23:50:31 +0900

    Create Blockchain class

---
+++ blockchain/blockchain.py
@@ -0,0 +1,38 @@
+import json
+import hashlib
+
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+from urllib.parse import urlparse
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set()
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash):
+        """
+        Create a new Block and add it to the chain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            "index": len(self.chain) + 1,
+            "timestamp": time(),
+            "transactions": self.current_transactions,
+            "proof": proof,
+            "previous_hash": previous_hash or self.hash(self.chain[-1])
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block
